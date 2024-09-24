053f4c3fc45243f2a7999ef6c81f1a7e10435012
GalloGGMC <gabriel.coutinho@sou.inteli.edu.br>
2024-09-24 13:39:05 -0300

    back concluido

---
+++ app.py
@@ -0,0 +1,125 @@
+import os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+from fastapi import FastAPI
+from modelCreator import createModel
+from tensorflow import keras
+import yfinance as yf
+from datetime import datetime, timedelta
+import numpy as np
+import pandas as pd
+import json
+
+def process_data(coin: str):
+    ticker = yf.Ticker(coin)
+
+    df = ticker.history(period="1d", interval='1m')
+    df = df.drop(columns=["Dividends","Stock Splits","High","Low","Open"])
+    df = df.rename(columns={"Close":"Value"})
+
+    with open('info.json', 'r') as openfile:
+        info = json.load(openfile)
+
+    df["Value"] = (df["Value"] - info[coin]["min"])/(info[coin]["max"] - info[coin]["min"])
+    values = df["Value"].values.reshape(-1,1)
+    x_p = np.array([values[-60+i] for i in range(60)]).reshape(1,60)
+
+    vol = df["Volume"].values[-1]
+
+    return [x_p, vol]
+
+def comparator(arr: list, vol:int):
+    if arr[0] > 0.1 and arr[1] > 0.1 and vol/274033664 < 0.1:
+        return "Buy"
+    
+    elif arr[0] < -0.1 and arr[1] < -0.1 and vol/274033664 < 0.2:
+        return "Sell"
+    
+    else:
+        return "Hold"
+    
+app = FastAPI()
+
+@app.get("/eth")
+def predictETH():
+    with open('logs.json', 'r') as openfile:
+        log = json.load(openfile)
+    log["typeConsult"].append("Predicao Etherium")
+    log["date"].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open('logs.json', 'w') as outfile:
+        json.dump(log, outfile)
+
+    model = keras.models.load_model("models/ETH-USD.h5")
+    X, vol = process_data("ETH-USD")
+    y = model.predict(X)
+    delta = [(X[0][-1] - X[0][-2])*10, (X[0][-1]-y[0][0])*10]
+
+    return comparator(delta, vol)
+
+@app.get("/btc")
+def predictETH():
+    with open('logs.json', 'r') as openfile:
+        log = json.load(openfile)
+    log["typeConsult"].append("Predicao Bitcoin")
+    log["date"].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open('logs.json', 'w') as outfile:
+        json.dump(log, outfile)
+        
+    model = keras.models.load_model("models/BTC-USD.h5")
+    X, vol = process_data("BTC-USD")
+    y = model.predict(X)
+    delta = [(X[0][-1] - X[0][-2])*10, (X[0][-1]-y[0][0])*10]
+
+    return comparator(delta, vol)
+
+@app.get("/hist_eth")
+def histETH():
+    with open('logs.json', 'r') as openfile:
+        log = json.load(openfile)
+    log["typeConsult"].append("Historico Etherium")
+    log["date"].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open('logs.json', 'w') as outfile:
+        json.dump(log, outfile)
+
+    ticker = yf.Ticker("ETH-USD")
+    df = ticker.history(period="5d", interval='1m')
+    df = df.drop(columns=["Dividends","Stock Splits","High","Low","Open","Volume"])
+    df = df.rename(columns={"Close":"Value"})
+    df.index = df.index.strftime("%d/%m/%Y %H:%M:%S")
+    j = df.to_json(orient='index')
+    parsed = json.loads(j)
+
+    return parsed
+
+@app.get("/hist_btc")
+def histBTC():
+    with open('logs.json', 'r') as openfile:
+        log = json.load(openfile)
+    log["typeConsult"].append("Historico Bitcoin")
+    log["date"].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open('logs.json', 'w') as outfile:
+        json.dump(log, outfile)
+
+    ticker = yf.Ticker("BTC-USD")
+    df = ticker.history(period="5d", interval='1m')
+    df = df.drop(columns=["Dividends","Stock Splits","High","Low","Open","Volume"])
+    df = df.rename(columns={"Close":"Value"})
+    df.index = df.index.strftime("%d/%m/%Y %H:%M:%S")
+    j = df.to_json(orient='index')
+    parsed = json.loads(j)
+
+    return parsed
+
+@app.get("/logs")
+def logs():
+    with open('logs.json', 'r') as openfile:
+        logT = json.load(openfile)
+
+    with open('logs.json', 'r') as openfile:
+        log = json.load(openfile)
+    log["typeConsult"].append("Logs")
+    log["date"].append(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
+    with open('logs.json', 'w') as outfile:
+        json.dump(log, outfile)
+
+
+    return logT
---
+++ modelCreator.py
@@ -0,0 +1,58 @@
+import numpy as np
+from datetime import datetime
+import yfinance as yf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+import json
+
+def createModel(coin: str):
+
+    ticker = yf.Ticker(coin)
+    df = ticker.history(period='max', interval='1m')
+
+    df = df.drop(columns=["Dividends","Stock Splits","High","Low","Open","Volume"])
+    df = df.rename(columns={"Close":"Value"})
+    with open('info.json', 'r') as openfile:
+        info = json.load(openfile)
+    info[coin]["max"] = df["Value"].max()
+    info[coin]["min"] = df["Value"].min()
+    with open('info.json', 'w') as outfile:
+        json.dump(info, outfile)
+    df["Value"] = (df["Value"] - df["Value"].min()) / (df["Value"].max() - df["Value"].min())
+
+    values = df["Value"].values.reshape(-1,1)
+    X_train = []
+    y_train = []
+    for i in range(60, len(df)-3000):
+        X_train.append(values[i-60:i, 0])
+        y_train.append(values[i, 0])
+
+    X_train, y_train = np.array(X_train), np.array(y_train)
+
+    X_test = []
+    y_test = []
+    for i in range(len(df)-2940, len(df)):
+        X_test.append(values[i-60:i, 0])
+        y_test.append(values[i, 0])
+
+    X_test, y_test = np.array(X_test), np.array(y_test)
+
+    model = Sequential()
+    model.add(LSTM(units=55,return_sequences=True,input_shape=(X_train.shape[1], 1)))
+    model.add(Dropout(0.15))
+    model.add(LSTM(units=55,return_sequences=True))
+    model.add(Dropout(0.15))
+    model.add(LSTM(units=55,return_sequences=True))
+    model.add(Dropout(0.15))
+    model.add(LSTM(units=55))
+    model.add(Dropout(0.15))
+    model.add(Dense(units=1))
+    model.compile(optimizer='adam',loss='mean_absolute_error')
+    model.fit(X_train,y_train,epochs=100,batch_size=32)
+
+    model.save(f"models/{coin}.h5")
+    
+
+if __name__ == "__main__":
+    createModel("ETH-USD")
+    createModel("BTC-USD")
